99d02f2719f1d733d24b8a4147fe87e6fec15bbf
Anton Onyshchenko <anton.onishchenko@gmail.com>
2014-03-14 16:01:01 +0100

    better key bindings for multiple cursors

--- init.el
+++ init.el
@@ -259,10 +259,10 @@
 
 
 (require 'multiple-cursors)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "H-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "H-a") 'mc/mark-all-like-this)
 
 (require 'ace-jump-mode)
 (define-key global-map (kbd "C-M-j") 'ace-jump-mode)
