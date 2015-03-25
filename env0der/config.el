bd815567f31ad49ff66d96350c49aab0d6c66e09
Anton Onyshchenko <anton.onyshchenko@xing.com>
2015-03-25 10:27:40 +0100

    Ruby indentation config

--- env0der/config.el
+++ env0der/config.el
@@ -9,3 +9,22 @@
       cperl-continued-statement-offset 4
       cperl-indent-parens-as-block t
       cperl-tab-always-indent t)
+
+;; better ruby intendation
+(setq ruby-deep-indent-paren nil)
+(setq enh-ruby-deep-indent-paren nil)
+
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
