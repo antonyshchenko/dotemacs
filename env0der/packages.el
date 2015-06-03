90b28bae0ebc066f07751a09e1120c58f44a150d
Anton Onyshchenko <anton.onishchenko@gmail.com>
2015-06-03 22:12:29 +0200

    C-w will backward-kill-word in when company mode is active

--- env0der/packages.el
+++ env0der/packages.el
@@ -227,6 +227,7 @@
       (define-key company-mode-map (kbd "M-j") 'company-select-next)
       (define-key company-mode-map (kbd "M-k") 'company-select-previous)
       (define-key company-active-map (kbd "M-l") 'company-complete-selection)
+      (define-key company-active-map (kbd "C-w") 'backward-kill-word)
       (define-key company-active-map (kbd "C-j") 'newline-and-indent-interactive))))
 
 (defun env0der/init-ruby-mode ()
